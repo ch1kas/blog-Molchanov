9b51719c75706f25b3b90d6924e03cbea18c0afc
Chingiz <chyngyz60390@gmail.com>
2020-11-10 11:28:41 +0600

    parts 7 and 8

--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import View
 
+from .forms import TagForm, PostForm
 from .models import Post, Tag
-from .utils import ObjectDetailMixin
+from .utils import ObjectDetailMixin, ObjectCreateMixin
 
 def posts_list(request):
     posts = Post.objects.all()
@@ -13,11 +14,21 @@ class PostDetail(ObjectDetailMixin, View):
     model = Post
     template = 'blog/post_detail.html'
 
+class PostCreate(ObjectCreateMixin, View):
+    model_form = PostForm
+    template = 'blog/post_create_form.html'
+
 
 class TagDetail(ObjectDetailMixin, View):
     model = Tag
     template = 'blog/tags_list.html'
 
+class TagCreate(ObjectCreateMixin, View):
+    model_form = TagForm
+    template = 'blog/tag_create.html'
+
+
 def tags_list(request):
     tags = Tag.objects.all()
     return render(request, 'blog/tags_list.html', context={'tags': tags})
+
