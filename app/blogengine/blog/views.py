c372c9d39203a964fca3597838c05d31daed6017
Chingiz <chyngyz60390@gmail.com>
2020-11-10 12:47:12 +0600

    parts 9 & 10

--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import View
 
+from django.urls import reverse
 from .forms import TagForm, PostForm
 from .models import Post, Tag
-from .utils import ObjectDetailMixin, ObjectCreateMixin
+from .utils import *
 
 def posts_list(request):
     posts = Post.objects.all()
@@ -18,6 +19,15 @@ class PostCreate(ObjectCreateMixin, View):
     model_form = PostForm
     template = 'blog/post_create_form.html'
 
+class PostUpdate(ObjectUpdateMixin, View):
+    model = Post
+    model_form = PostForm
+    template = 'blog/post_update_form.html'
+
+class PostDelete(ObjectDeleteMixin, View):
+    model = Post
+    template = 'blog/post_delete_form.html'
+    redirect_url = 'posts_list_url'
 
 class TagDetail(ObjectDetailMixin, View):
     model = Tag
@@ -27,6 +37,15 @@ class TagCreate(ObjectCreateMixin, View):
     model_form = TagForm
     template = 'blog/tag_create.html'
 
+class TagUpdate(ObjectUpdateMixin, View):
+    model = Tag
+    model_form = TagForm
+    template = 'blog/tag_update_form.html'
+
+class TagDelete(ObjectDeleteMixin, View):
+    model = Tag
+    template = 'blog/tag_delete_form.html'
+    redirect_url = 'tags_list_url'
 
 def tags_list(request):
     tags = Tag.objects.all()
